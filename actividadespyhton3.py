4c961a11fcb9b5908cd822276a0a4d2a1c93bf24
barbararaquelperez <55531065+barbararaquelperez@users.noreply.github.com>
2019-10-04 22:39:33 +0200

    Add files via upload

---
+++ actividadespyhton3.py
@@ -0,0 +1,10 @@
+x=range(100)
+range(100)
+list(x)
+print"pares e impares"
+num=int(raw_input("introduce algun numero entero :"))
+if num%2==0:
+    print"este numero es par"
+if num%2!=0:
+    print"este numero es impar"
+    
