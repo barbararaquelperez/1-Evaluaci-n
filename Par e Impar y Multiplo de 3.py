8aac93e3bb8c44b2cd1a343c20270e9c4206028b
barbararaquelperez <55531065+barbararaquelperez@users.noreply.github.com>
2019-10-15 11:49:02 +0200

    Add files via upload

---
+++ Par e Impar y Multiplo de 3.py
@@ -0,0 +1,15 @@
+def ejemplo_for():
+       #x es una variable que toma los valores comprendidos en
+       #el rango
+       i=input("Introduzca un numero: ")
+
+       if(i%2==0):
+        if(i%3==0):
+               print i, "es par"
+               print i, "es multiplo de 3"
+        else:
+               print i, "es impar"
+               print i, "no es multiplo de 3"
+ejemplo_for()
+
+     
