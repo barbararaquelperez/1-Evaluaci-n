30c4111d53015a1a6c39d90b7a3d9ce549db92fc
barbararaquelperez <55531065+barbararaquelperez@users.noreply.github.com>
2019-10-17 09:08:53 +0200

    Add files via upload

---
+++ Primo.py
@@ -0,0 +1,10 @@
+def primo():
+	numero=input ("say me your number")
+  	primo= True
+	for i in range (2, numero):
+		if(numero%i==0):
+			primo=False
+	if(primo==True):
+		print "es primo"
+	else:
+		print "no es primo"
