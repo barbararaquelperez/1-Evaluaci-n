ae0dadb9f71b8b01db7ae816fd28eacbe071852c
barbararaquelperez <55531065+barbararaquelperez@users.noreply.github.com>
2019-10-22 11:34:38 +0200

    Add files via upload

--- Ej 1.py
+++ Ej 1.py
@@ -1,12 +1,25 @@
 #en este programa multiplicaremos los numeros que el usuario introduzca
-def mmultiplicacion():
+def multiplicacion():
        a=input ("Introduzca un numero: ")
        b=input ("Intoduzca otro numero: ")
        c=input ("Introduzca otro numero: ")
        d=input ("Introduzca otro numero: ")
+       i=a*b*c*d
        if(a==0):
-              print "i= ", i, "su producto es 0"
+              print "i= ",i , "su producto es 0"
        else:
               if(b==0):
-                     print "i= ", i, "su numero es 0"
+                     print "i= ",i , "su producto es 0"
+              else:
+                     if(c==0):
+                            print "i= ",i , "su producto es 0"
+                     else:
+                            if(d==0):
+                                   print "i= ", i , "su producto es 0"
+                            else:
+                                   print "i= ", i
+              
+       
+              
+       
 multiplicacion() 
