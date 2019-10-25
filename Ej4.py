aa2ba72fe54a3538e5cd5a51fbc81d9b074559b0
barbararaquelperez <55531065+barbararaquelperez@users.noreply.github.com>
2019-10-25 09:25:37 +0200

    Add files via upload

---
+++ Ej4.py
@@ -0,0 +1,41 @@
+# En este programa insertas un numero del 1 al 12 y el programa te dira el mes que es 
+def mes():
+       x=input("Introduzca un numero del 1 al 12 para conocer el mes del año: ")
+       if(x==1):
+              print "Es enero"
+       else:
+              if(x==2):
+                      print "Es febrero"
+              else:
+                     if(x==3):
+                            print "Es marzo"
+                     else:
+                            if(x==4):
+                                   print "Es abril"
+                            else:
+                                   if(x==5):
+                                          print "Es mayo"
+                                   else:
+                                          if(x==6):
+                                                 print"Es junio"
+                                          else:
+                                                 if(x==7):
+                                                        print"Es julio"
+                                                 else:
+                                                        if(x==8):
+                                                               print "Es agosto"
+                                                        else:
+                                                               if(x==9):
+                                                                      print "Es septiembre"
+                                                               else:
+                                                                      if(x==10):
+                                                                             print"Es octubre"
+                                                                      else:
+                                                                             if(x==11):
+                                                                                    print"Es noviembre"
+                                                                             else:
+                                                                                    if(x==12):
+                                                                                           print"Es diciembre"
+                                          
+mes()
+              
