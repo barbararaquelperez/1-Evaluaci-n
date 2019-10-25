5041a2d8b835e498e6b0c3299fe8d914173fd5da
barbararaquelperez <55531065+barbararaquelperez@users.noreply.github.com>
2019-10-25 09:38:57 +0200

    Add files via upload

---
+++ Ej5.py
@@ -0,0 +1,32 @@
+def mes():
+       x=input("Introduce un dia: ")
+       y=input("Introduce un mes: ")
+       z=input("Introduce un año: ")
+       if(y==1):
+              print x, "de enero de", z
+       if(y==2):
+              print x, "de febrero de", z
+       if(y==3):
+              print x, "de marzo de", z
+       if(y==4):
+              print x, "de abril de", z
+       if(y==5):
+              print x, "de mayo de", z
+       if(y==6):
+              print x, "de junio de", z
+       if(y==7):
+              print x, "de julio de", z
+       if(y==8):
+              print x, "de agosto de", z
+       if(y==9):
+              print x, "de septiembre de", z
+       if(y==10):
+              print x, "de octubre de", z
+       if(y==11):
+              print x, "de noviembre de", z
+       if(y==12):
+              print x, "de diciembre de", z
+mes()
+
+
+       
